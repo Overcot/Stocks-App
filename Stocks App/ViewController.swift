b9d33788040954b004363edeb443326afb7fa67b
Alexey Ivashko <mcivashko@gmail.com>
2018-09-12 08:20:53 +0300

    Initial Commit

---
+++ Stocks App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stocks App
+//
+//  Created by Alex Ivashko on 12.09.2018.
+//  Copyright © 2018 Alex Ivashko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
